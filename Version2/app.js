65fccb611417562cc6455c3f6b8ffe6b65791ae6
jlvillalobosj <jlvillalobosj@unal.edu.co>
2024-11-02 20:27:40 -0500

    Add a list since javascript and add a module screen to create new projects

--- Version2/app.js
+++ Version2/app.js
@@ -1,34 +1,29 @@
-var API_URL = 'http://localhost:8080/CourseSearch/';
-let courses = {};
+function addRowTableProject(dataProject, counter) {
+    document.getElementById("tableProjects").innerHTML += buildRowTr(dataProject, counter);
+}
 
-const formCreateSubject = document.querySelector('form')
 
-var optionCreateEdit = true
-var idSubject = 0
+
+function readTextFile(file, callback) {
+  var rawFile = new XMLHttpRequest();
+  rawFile.overrideMimeType("application/json");
+  rawFile.open("GET", file, true);
+  rawFile.onreadystatechange = function() {
+      if (rawFile.readyState === 4 && rawFile.status == "200") {
+          callback(rawFile.responseText);
+      }
+  }
+  rawFile.send(null);
+}
 
 
 function createTable(){
-  $.getScript("Service.js", () => {
-    GET(API_URL + "courses")
-    .then(courses => {
-      document.getElementById("table").innerHTML = ''; 
-      document.getElementById("table").innerHTML = `<thead><tr class="w3-blue-gray">
-      <th>Código</th>
-      <th>Nombre</th>
-      <th>Créditos</th>
-      <th>Cupos disponibles</th>
-      <th>Acciones</th>
-      </tr></thead>`; 
-      for (let course in courses){
-        document.getElementById("table").innerHTML += `<tr>
-        <td >${courses[course].codeSubject} </td>
-        <td id="nameSubject${courses[course].codeSubject}">${courses[course].nameSubject} </td>
-        <td>${courses[course].creditsSubject}</td>
-        <td>${courses[course].quotesSubject}</td>
-        <td> <button onclick="OpenModalEdit(${courses[course].codeSubject})" class="w3-button w3-circle" data-bs-toggle="tooltip" title="Editar" ><i class="fa fa-pencil w3-large " ></i></button> 
-        <button class="w3-button w3-circle" onclick="OpenModalDelete(${courses[course].codeSubject})" data-bs-toggle="tooltip" title="Eliminar"><i class="fa  fa-trash w3-large"></i></button></td>
-        </tr>`; 
-      }
+  readTextFile("./../test_Json/test_Project_Table.json", function(text){
+    var data = JSON.parse(text);
+    var counter = 0;
+    data.forEach(element => {
+      addRowTableProject(element, counter)
+      counter++;
     });
   });
 };
@@ -36,55 +31,71 @@ function createTable(){
 
 createTable()
 
-const createSubject = () => {
-  const formData = new FormData(document.querySelector('#formCreateCourse'));
 
-  if(!formData.get('nameSubject').length || !formData.get('creditsSubject') || !formData.get('quotesSubject')) {
-    document.querySelector('#msgFormAdd').innerHTML = '* Llena todos los campos';
-    return;
-  }
-  document.querySelector('#msgFormAdd').innerHTML = '';
+function buildRowTr(element, counter){
 
-  const subject = {
-    nameSubject: formData.get('nameSubject'),
-    codeCreateSubject: formData.get('codeSubject'),
-    creditsSubject: formData.get('creditsSubject'),
-    quotesSubject: formData.get('quotesSubject'),
-    descriptionSubject: formData.get('descriptionSubject'),
+  var colorstate = 'gray';
+  var colorRow = 'white';
+  if(counter%2==0){
+  colorRow = 'white'
+  }else{
+  colorRow = '#f2f2f2'
   }
 
-
-  if (optionCreateEdit){  
-    $.getScript("Service.js", () => {
-    POST(API_URL + "create-course", subject)
-    .then(res =>{
-      document.querySelector('#formCreateCourse').reset();
-      createTable();
-      alertify.success('Se creó el curso '+ subject.nameSubject)
-    }) 
-    .catch(error => {
-      console.log(error)
-      alertify.error('Error')
-    })
-    });
-  } else {
-    $.getScript("Service.js", () => {
-      PUT(API_URL + "update-course/" + idSubject, subject)
-      .then(res =>{
-        document.querySelector('#formCreateCourse').reset();
-        
-      alertify.success('Se editó el curso '+ idSubject)
-        createTable();
-      }) 
-      .catch(error => {
-        console.log(error)
-        alertify.error('Error')
-      })
-      });
+  switch (element.state) {
+    case "Finished":
+      colorstate = 'green'
+      break;
+    case "Process":
+      colorstate = 'yellow'
+      break;
+    case "Not started":
+      colorstate = 'red'
+      break;
+    default:
+      colorstate = 'gray'
+      break;
   }
-  document.getElementById('modalCreateCourse').style.display='none'
-
+    console.log(element.name)
+  return `<tr style="background-color: ${colorRow};">
+            <td>${element.name}</td>
+            <td>${element.description}       
+              <div class="w3-dropdown-hover">Tokyo
+                <div class="w3-dropdown-content w3-card-4" style="width:250px">
+                  <img src="img_tokyo.jpg" alt="Tokyo" style="width:100%">
+                  <div class="w3-container">
+                    <p>Tokyo is the capital city of Japan.</p>
+                    <p>13 million inhabitants.</p>
+                  </div>
+                </div>
+              </div>
+            </td>
+            <td>${element.members}</td>
+            <td class="w3-text-${colorstate}">${element.state}</td>
+            <td>
+              <div class="w3-display-container" style="display: flex; align-items: center;">
+                <div class="w3-tooltip" style="position: relative;">
+                  <i class="fas fa-eye w3-hover-text-blue" style="margin-right: 20%;"></i>
+                  <span style="position:absolute; left:0; bottom:78%;" class="w3-text w3-tag w3-round w3-dark-grey w3-small w3-animate-opacity">
+                      View
+                  </span>
+                </div>
+          
+                <div class="w3-tooltip" style="position: relative; margin-left: 15%;">
+                  <i class="fas fa-trash-alt w3-hover-text-red" style="margin-left: 40%;"></i>
+                  <span style="position:absolute; left:0; bottom:78%;" class="w3-text w3-tag w3-round w3-dark-grey w3-small w3-animate-opacity">
+                      Delete
+                  </span>
+                </div>
+              </div>
+            </td>
+          </tr>`
 }
+
+
+
+
+
 
 function OpenModalEdit(id) {
   optionCreateEdit = false
@@ -102,40 +113,20 @@ function OpenModalEdit(id) {
     });
   });
   //document.getElementById('nameSubject').value = document.getElementById('nameSubject'+ idSubject).innerText;
-  document.getElementById('modalCreateCourse').style.display='block'
+  document.getElementById('modalCreateProject').style.display='block'
   
 }
 
 function OpenModalCreate() {
   optionCreateEdit = true
   var textButton = document.getElementById('Guardar_Editar');
-  textButton.innerText = "Guardar";
-  document.getElementById('nameSubject').value = '';
-  document.getElementById('creditsSubject').value = '';
-  document.getElementById('quotesSubject').value = '';
-  document.getElementById('descriptionSubject').value = '';
+  textButton.innerText = "Save";
+  document.getElementById('nameProject').value = '';
+  document.getElementById('membersProject');
+  document.getElementById('stateProject');
+  document.getElementById('descriptionProject').value = '';
 
-  document.getElementById('modalCreateCourse').style.display='block'
+  document.getElementById('modalCreateProject').style.display='block'
 }
-
-function OpenModalDelete(id) {
-  idSubject = id;
-  document.getElementById('modalDeleteCourse').style.display='block' 
-}
-
-function DeleteElement() { 
-  $.getScript("Service.js", () => {
-    Delete(API_URL + "delete-course/" + idSubject)
-    createTable();
-    document.getElementById('modalDeleteCourse').style.display='none'
-    alertify.error('Se eliminó el curso')
-  });
-}
-
-
-
-
-
-
 
 
